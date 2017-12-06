237f09b7e7dbb93057c7517c069994ae3e9c6d3a
Rob Wu <rob@robwu.nl>
2017-12-07 00:32:18 +0100

    Default extensions.showMismatchUI to false
    To disable automatic updating of legacy add-ons when the browser is
    updated to a newer version.

--- enablelegacy-prefs.js
+++ enablelegacy-prefs.js
@@ -3,9 +3,17 @@
 pref("general.config.filename", "mozilla.cfg");
 pref("general.config.obscure_value", 0);
 
+// Do not automatically upgrade legacy add-ons when upgrading the browser.
+// (Note: To avoid unwanted legacy -> WebExtension upgrades,
+//  visit about:config and set extensions.update.autoUpdateDefault to false ).
+pref("extensions.showMismatchUI", false);
+
+// When this preference is toggled, legacy add-ons are automatically re-enabled.
+// Non-restartless add-ons may require a restart of the browser.
+// Non-multiprocess-compatible add-ons may disable e10s.
 // pref("extensions.legacy.enabled", true);
-// pref("extensions.update.autoUpdateDefault", false);
 // pref("xpinstall.signatures.required", false);
+// (Note: uncommenting won't work because app/profile/firefox.js also sets them)
 
 // Maybe even the following, to allow manual execution of JS code in the global console.
 // pref("devtools.chrome.enabled", true);
